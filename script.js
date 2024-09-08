1e5c4a746b7dbc40098d8911ab62f1e776250aa2
Johan Mesa <Johanotr@gmail.com>
2024-09-08 09:59:33 -0400

    Refactor js code

--- script.js
+++ script.js
@@ -1,166 +1,180 @@
+const GameBoard = (function (){
+    let board = [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ];
 
-// Create game board
+    const getBoard = () => board;
 
-const gameBoardModule = (function() {
-        let gameboard = [
-                         "","","",
-                         "","","",
-                         "","",""
-                        ];
-
-        const placeToken = (symbol, index) => {
-            if (gameboard[index] != "") {
-                return false;
-            }
-            else {
-            return gameboard[index] = symbol;
-            }
+    const placeToken = (index, token) => {
+        if (board[index] != "") {
+            console.log("Token already placed here.");
+            return false;
         }
-
-        const clearBoard = () => {
-            return gameboard = ["","","","","","","","",""];
+        else {
+            board[index] = token;
         }
+        
+    };
 
-        const showBoard = () => gameboard;
+    const clearBoard = () => board = ["", "", "","", "", "","", "", ""];
 
-        const checkWinPattern = (token) => {
-            // Check horizontal
-            for(let i = 0; i < 9; i += 3) {
+    const checkForWinner = (token) => {
+         // Check horizontal
+         for(let i = 0; i < 9; i += 3) {
 
-                if (gameboard[i] === token && gameboard[i+1] === token && gameboard[i+2] === token) {
-                    return true;
-                } else {
-                    continue;
-                } 
-            }
-            // Check Vertical
-            for (let i = 0; i < 4; i++) {
-               if (gameboard[i] === token && gameboard[i+3] == token && gameboard[i+6] == token) {
+            if (board[i] === token && board[i+1] === token && board[i+2] === token) {
                 return true;
-               }
-               else {
+            } else {
                 continue;
-               }
-            }
-            //Check Diagnol
-            for (let i = 0; i < 1; i++) {
-                if(gameboard[i] === token && gameboard[i+4] === token && gameboard[i+8] === token) {
-                    return true;
-                }
-                else if(gameboard[i + 2] === token && gameboard[i+4] === token && gameboard[i+6] === token) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
             } 
         }
+        // Check Vertical
+        for (let i = 0; i < 4; i++) {
+           if (board[i] === token && board[i+3] == token && board[i+6] == token) {
+            return true;
+           }
+           else {
+            continue;
+           }
+        }
+        //Check Diagnol
+        for (let i = 0; i < 1; i++) {
+            if(board[i] === token && board[i+4] === token && board[i+8] === token) {
+                return true;
+            }
+            else if(board[i + 2] === token && board[i+4] === token && board[i+6] === token) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        } 
+    }
 
-        return {showBoard, placeToken, clearBoard, checkWinPattern} 
+    return {getBoard, placeToken, clearBoard, checkForWinner}
 })();
 
-// player function factory
-
-function createPlayer (name, token) {
+function CreatePlayer (name, token, id) {
+    let wins = 0,
+        losses = 0,
+        ties = 0
     
-    let wins = 0;
-    let loss = 0;
-
     const addWin = () => wins++;
-    const addLoss = () => loss++;
+    const addLoss = () => losses++;
+    const addTie = () => ties++;
 
+    const getWin = () => wins;
+    const getLoss = () => losses;
+    const getTie = () => ties;
 
-    return {name, token, addWin, addLoss}
+    return {name, token, addWin, addLoss, addTie, getWin, getLoss, getTie, id}
+}
+
+const Players = (function () {
+    let players = [];
+
+    let activePlayer;
+    let inactivePlayer;
+
+    const addPlayers = (player1Name, player2Name) => {
+       let player1 = CreatePlayer(player1Name, "x", 1);
+       let player2 = CreatePlayer(player2Name, "o", 2);
+
+        players = [player1, player2];
+
+        activePlayer = players[0];
+        inactivePlayer = players[1];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const switchActivePlayers = () => {
+        inactivePlayer = activePlayer;
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        
+    }
+
+    const getInactivePlayer = () => inactivePlayer;
+
+    return{addPlayers, getActivePlayer, switchActivePlayers, getInactivePlayer};
+
+})();
+
+const GameController = (function () {
+
+    const playRound = (cell, activePlayer) => {
+        
+        if(GameBoard.placeToken(cell,activePlayer.token) != false) {
+            
+            if(GameBoard.checkForWinner(activePlayer.token)){
+                console.log(` ${activePlayer.name} has have won`);
+                GameBoard.clearBoard();
+                activePlayer.addWin();
+                Players.getInactivePlayer().addLoss();
+            }
+            Players.switchActivePlayers();   
+        }
+        else {
+            return false;
+        }
+    
+    }
+
+    return {playRound}
+
+})();
+
+function ScreenController () {
+    const $startScreen = document.querySelector("#Start-Screen"),
+        $createPlayerScreen = document.querySelector("#Create-Character-Screen"),
+        $addNameForm = document.querySelector("#create-player-form"),
+        $playingScreen = document.querySelector("#Playing-Screen"),
+        $cells = document.querySelectorAll("td");
+
+    const updateScreen = () => {
+        for(let i=0; i < 9; i++) {
+            $cells[i].dataset.value = GameBoard.getBoard()[i];
+        }
+    }
+
+    function goToCreatePlayerScreen (e) {
+        if(e.target.id === "Start-Screen-btn") {
+            $startScreen.classList.add("disable");
+            $createPlayerScreen.classList.remove("disable");
+
+        }
+    }
+    $startScreen.addEventListener('click', goToCreatePlayerScreen);
+
+    function addPlayersToPlayers (e) {
+        e.preventDefault();
+        Players.addPlayers(e.srcElement[0].value, e.srcElement[1].value);
+        $createPlayerScreen.classList.add("disable");
+        $playingScreen.classList.remove("disable");
+    }
+    $addNameForm.addEventListener("submit", addPlayersToPlayers);
+
+    function changeTurn () {
+        $cells.forEach((cell) => {
+            cell.dataset.playerTurn = `player${Players.getActivePlayer().id}`;
+        })
+    }
+    function addTokenToScreen (cell) {
+        //cell.dataset.value = Players.getActivePlayer().token;
+        
+        GameController.playRound(cell.dataset.index, Players.getActivePlayer());
+        changeTurn();
+        updateScreen();
+    }
+
+    $cells.forEach((cell) => {
+        cell.addEventListener('click', () => addTokenToScreen(cell));
+    })
+
 
 }
 
-// Display game to DOM
-const screenController = (function () {
-    const $startBtn = document.querySelector("#Start-Screen-btn"),
-          $createPlayerScreen = document.querySelector("#Create-Character-Screen"),
-          $addNameForm = document.querySelector("#create-player-form"),
-          $playingScreen = document.querySelector("#Playing-Screen");
-    
-    return {$startBtn, $createPlayerScreen,$addNameForm,$playingScreen}
-})();
 
-//game flow module
-
-const gameController = (function (){
-    
-
-    const setUpGame = () => {
-
-        //Start Screen
-        screenController.$startBtn.addEventListener('click', (e) =>
-        {
-            setTimeout(e.target.parentElement.className = "disable", 1000);
-            screenController.$createPlayerScreen.className = "";
-        });
-
-        screenController.$addNameForm.addEventListener("submit", (e) => {
-            e.preventDefault();
-
-            player1 = createPlayer(e.target[0].value);
-            player2 = createPlayer(e.target[1].value);
-
-            setTimeout(screenController.$createPlayerScreen.className = "disable", 1000);
-            screenController.$playingScreen.className = "";
-
-        });
-
-        return {player1, player2}
-    }
-
-    const startGame = () => {
-        //Game all turns
-        for (let i = 0; i < 5; i++){
-            
-            // holds players desire spot to place token
-            let index;
-
-            // First player turn
-            while(!gameBoardModule.placeToken(player1.token, index)){
-                index = prompt(`${player1.name}'s turn`);
-            }
-            //end game if player 1 won
-            if (gameBoardModule.checkWinPattern(player1.token)) {
-                gameBoardModule.clearBoard();
-                player1.addWin();
-                player2.addLoss();
-                break;
-            }
-            console.log(gameBoardModule.showBoard());
-
-            //run if max number of turns reached
-            if(i === 4){
-                gameBoardModule.clearBoard();
-                break;}
-
-            //Second player turn
-            while(!gameBoardModule.placeToken(player2.token, index)){
-                index = prompt(`${player2.name}'s turn`);
-            }
-            //end game if player 2 won
-            if(gameBoardModule.checkWinPattern(player2.token)) {
-                gameBoardModule.clearBoard();
-                player2.addWin();
-                player1.addLoss();
-                break;
-            }
-            console.log(gameBoardModule.showBoard());
-        }
-
-
-    }
-    
-    const playAgain = () => startGame();
-
-    const restartGame = () => createPlayers();
-
-    return {setUpGame, startGame, playAgain, restartGame}
-})();
-
-
-
-gameController.setUpGame();
+ScreenController();
